d7729a11fc8056fe6f8b7c7a69245bacac478153
NobreJanio <janionobrega@hotmail.com>
2024-01-03 16:15:32 -0300

    cards linkados

--- src/components/3-main/myProjects.js
+++ src/components/3-main/myProjects.js
@@ -1,22 +1,22 @@
 export const myProjects = [
 
-  { projectTitle: "UNES",                 category: ["css"], imgPath: "./4.png", descrition: "Meu primeiro projeto de uma universidae fictícia." },
-  { projectTitle: "Anna Bella",           category: ["css"], imgPath: "./1.png", descrition: "Perfil de uma modelo fictícia com o layout feito em tabelas." },
-  { projectTitle: "TecBlog",              category: ["css"], imgPath: "./3.png", descrition: "Página no estilo de blog." },
-  { projectTitle: "Notícias Cidade",      category: ["css"], imgPath: "./2.png", descrition: "Página de notícias fictias feito em colunas." }, 
-  { projectTitle: "Restaurante (Modelo)", category: ["css"], imgPath: "./5.png", descrition: "Modelo de página de um restaurante fictício"  },
+  { projectTitle: "UNES",                 category: ["css"], imgPath: "./4.png", descrition: "Meu primeiro projeto de uma universidae fictícia.", link: "https://6595a71669cbf1090d8ff742--hilarious-torte-d40002.netlify.app/"  },
+  { projectTitle: "Anna Bella",           category: ["css"], imgPath: "./1.png", descrition: "Perfil de uma modelo fictícia com o layout feito em tabelas.", link: "https://6595a89e847cc9006827db35--capable-squirrel-a738ee.netlify.app/"  },
+  { projectTitle: "TecBlog",              category: ["css"], imgPath: "./3.png", descrition: "Página no estilo de blog.", link: "https://6595a82791a9f4059cf726c6--phenomenal-rugelach-a0f304.netlify.app/" },
+  { projectTitle: "Notícias Cidade",      category: ["css"], imgPath: "./2.png", descrition: "Página de notícias fictias feito em colunas.", link: "https://6595a871a056530452f24ac4--transcendent-elf-4445c8.netlify.app/" }, 
+  { projectTitle: "Restaurante (Modelo)", category: ["css"], imgPath: "./5.png", descrition: "Modelo de página de um restaurante fictício", link: "https://main--restaurantenobre.netlify.app/"  },
 
-  { projectTitle: "Conversor de Moedas",          category: ["js"], imgPath: "./6.png", descrition: "Conversor de moedas de reais para dolar, euro, libra e bitcoin" },
-  { projectTitle: "Dia dos Namorados",            category: ["js"], imgPath: "./7.png", descrition: "Projeto pessoal feito para minha namorada" },
-  { projectTitle: "Relógio Digital & Smartwatch", category: ["js"], imgPath: "./10.png", descrition: "Relógio digital e Smartwatch com API " },
-  { projectTitle: "Calculadora",                  category: ["js"], imgPath: "./11.png", descrition: "Calculadora apenas praticando o Javascript" },
-  { projectTitle: "Lista de tarefas",             category: ["js"], imgPath: "./13.png", descrition: "Lista de tarefas com a função de remover" },
-  { projectTitle: "CRUD",                         category: ["js"], imgPath: "./12.png", descrition: "CRUD em Javascript" },
-  { projectTitle: "Instagram",                    category: ["js"], imgPath: "./14.png", descrition: "Layout da página de login do Instagram" },
+  { projectTitle: "Conversor de Moedas",          category: ["js"], imgPath: "./6.png", descrition: "Conversor de moedas de reais para dolar, euro, libra e bitcoin", link: "https://6595a92d0d0f0a006f579e4e--iridescent-youtiao-eb32a6.netlify.app/" },
+  { projectTitle: "Dia dos Namorados",            category: ["js"], imgPath: "./7.png", descrition: "Projeto pessoal feito para minha namorada", link: "https://6595a9afa5d8a2006697edb9--comforting-truffle-fd6f65.netlify.app/" },
+  { projectTitle: "Relógio Digital & Smartwatch", category: ["js"], imgPath: "./10.png", descrition: "Relógio digital e Smartwatch com API", link: "https://6595a8e8bd431f0083a7ab4e--enchanting-bonbon-921840.netlify.app/" },
+  { projectTitle: "Calculadora",                  category: ["js"], imgPath: "./11.png", descrition: "Calculadora apenas praticando o Javascript", link: "https://6595a9550372bf008768c254--chipper-cheesecake-be28fb.netlify.app/" },
+  { projectTitle: "Lista de tarefas",             category: ["js"], imgPath: "./13.png", descrition: "Lista de tarefas com a função de remover", link: "https://6595aafbf083ba0084096861--stunning-lebkuchen-a7f570.netlify.app/" },
+  { projectTitle: "CRUD",                         category: ["js"], imgPath: "./12.png", descrition: "CRUD em Javascript", link: "https://6595aa9e847cc9006627dba1--dashing-bublanina-bbdca9.netlify.app/" },
+  { projectTitle: "Instagram",                    category: ["js"], imgPath: "./14.png", descrition: "Layout da página de login do Instagram", link: "https://instadevclub.netlify.app/" },
 
 
 
-  { projectTitle: "angular project", category: ["angular"], imgPath: "./8.png", descrition: "Layout de um e-commerce feito em Angular e Express" },
+  { projectTitle: "angular project", category: ["angular"], imgPath: "./8.png", descrition: "Layout de um e-commerce feito em Angular e Express", link: "https://github.com/NobreJanio/projeto-angular1.git" },
 
-  { projectTitle: "react project", category: ["react"], imgPath: "./9.png", descrition: "Próprio porffolio feito em React + Vite" }, 
+  { projectTitle: "react project", category: ["react"], imgPath: "./9.png", descrition: "Próprio porffolio feito em React + Vite", link: "https://github.com/NobreJanio/Portfolio.git" }, 
 ];
