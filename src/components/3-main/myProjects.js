e3ccc7d7829172b8e4443d441eb39d5b9524b4d2
NobreJanio <janionobrega@hotmail.com>
2024-11-10 14:47:58 -0300

    link

--- src/components/3-main/myProjects.js
+++ src/components/3-main/myProjects.js
@@ -25,7 +25,7 @@ export const myProjects = [
   { projectTitle: "react project", category: ["react"], imgPath: "./9.png", descrition: "Próprio porffolio feito em React + Vite", link: "https://github.com/NobreJanio/Portfolio.git" },
   { projectTitle: "react project", category: ["react"], imgPath: "./17.png", descrition: "Ecommerce com Strapi", link: "https://ecommerce-90e52.web.app" },
   { projectTitle: "react project", category: ["react"], imgPath: "./18.png", descrition: "Ecommerce com redux, redux tookit, slick, react-router-dom", link: "https://lojafrutas-e2efc.web.app" },
-  { projectTitle: "react project", category: ["react"], imgPath: "./23.png", descrition: "Burgeria frontend e backend com styled-components, jwt, strepi, MongoDB, Postgresql, docker ... ", link: "https://blastburger-5f44f.web.app/home" },
+  { projectTitle: "react project", category: ["react"], imgPath: "./23.png", descrition: "Burgeria frontend e backend com styled-components, jwt, strepi, MongoDB, Postgresql, docker ... ", link: "https://blastburger-5f44f.web.app" },
 
   //{ projectTitle: "vue project", category: ["vue"], imgPath: "./21.png", descrition: "Restaurante Tokyo Sushi", link: "https://tokyo-sushi-d96d6.web.app" },
   { projectTitle: "vue project", category: ["vue"], imgPath: "./22.png", descrition: "Loja de roupas", link: "https://ecommerce-roupas-vue.web.app" },
